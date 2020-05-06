784621153d67c627a5c2d93e1615f2031d120c20
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-05-06 12:36:57 +0000

    setup responsive bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+// eslint-disable-next-line
+// eslint-no-unused-vars
 import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 import './App.css';
 
@@ -32,7 +38,20 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Daniel</Navbar.Brand>
+            <Navbar.Toggle arie-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          
+        </Container>
       </Router>
     );
   }
