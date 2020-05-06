85f1ae09bd68bd43e698d3465433c05840c7052e
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-05-06 11:36:01 +0000

    setup react router for nav links

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Daniel Larbi Addo',
+      headerLinks: [
+        { title: 'Home', path: '/'},
+        { title: 'About', path: '/about'},
+        { title: 'Contact', path: '/contact'}
+        
+      ],
+      home: {
+        title: 'My amazing portfolio',
+        subTitle: 'Check out my projects',
+        text: 'click for more'
+      },
+      about: {
+        title: 'About me',
+      },
+      contact: {
+        title: 'Lets talk',
+      }
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        
+      </Router>
+    );
+  }
+  
 }
 
 export default App;
