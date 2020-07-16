26c7685bad58e73551cab9633cba96efaa38a583
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-07-16 07:28:51 +0000

    configure node js server

---
+++ index.js
@@ -0,0 +1,56 @@
+let express = require('express');
+let router = express.Router();
+let nodemailer = require('nodemailer');
+let cors = require('cors');
+const creds = require('./config');
+
+let transport = {
+    host: 'smtp.example.com', // Don’t forget to replace with the SMTP host of your provider
+    port: 587,
+    auth: {
+    user: creds.USER,
+    pass: creds.PASS
+  }
+}
+
+let transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready to take messages');
+  }
+});
+
+router.post('/send', (req, res, next) => {
+  let name = req.body.name
+  let email = req.body.email
+  let message = req.body.message
+  let content = `name: ${name} \n email: ${email} \n message: ${message} `
+
+  let mail = {
+    from: name,
+    to: 'info@daniellarbiaddo.com',  // Change to email address that you want to receive messages on
+    subject: 'New Message from Contact Form',
+    text: content
+  }
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail'
+      })
+    } else {
+      res.json({
+       status: 'success'
+      })
+    }
+  })
+})
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/', router)
+app.listen(3002)
