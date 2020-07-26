e8285901181aee12c3552b1938e1b6eb8afa79d2
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-07-26 18:04:12 +0000

    add and install typedjs, react-reveal animations

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Slide from 'react-reveal/Slide';
+import Flash from 'react-reveal/Flash';
 
 import {
   FaHtml5, FaCss3Alt, FaDatabase, FaJs, FaReact, FaGithub,
@@ -7,7 +9,11 @@ import {
 } from 'react-icons/fa';
 import './style.css';
 
-import { gsap } from 'gsap';
+import {gsap, Power3, Power4} from 'gsap';
+// import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import Typed from 'typed.js';
+
 
 import fbImg from '../../Images/fb.png';
 import todoImg from '../../Images/todo.png';
@@ -22,16 +28,34 @@ import libImg from '../../Images/lib.png';
 
 export default class HomePage extends Component {
 
+  constructor(props) {
+    super(props);
+
+    gsap.registerPlugin(ScrollTrigger);
+
+  }
+
   componentDidMount() {
 
     gsap.from('.anim', { opacity: 0, duration: 1, y: -50, stagger: 0.6});
-    gsap.from('.animLeft', { opacity: 0, duration: 1, x: 150});
-    gsap.from('.animRight', { opacity: 0, duration: 1, x: -150, y: 200, stagger: .5})
-        // .to( {opacity: 1, duration: 1, x: -50, stagger: 0.7})
-    // gsap.from('.designer', { opacity: 0, duration: 1, y: -30, stagger: 0.6, ease: 'bounce', delay: 1.2})
-    gsap.to('.creator', { opacity: 0, duration: 1, y: -40, stagger: 0.5, ease: 'elastic(1, 0.3)'})
-    gsap.to('.thinker', { opacity: 0, duration: 1, y: -50, stagger: 0.6, delay: 1.1})
+    gsap.from('.animLeft', { opacity: 0, duration: 1, x: 150,
+      y: -50, ease: Power3.easeInOut
+    });
+    gsap.from('.animRight', { opacity: 0, duration: 1, x: -150, y: 200, stagger: .5, ease:"back.out(2)"});
+    gsap.from('.animTop', { opacity: 0, duration: 1, x: 550,
+       ease: Power4.easeOut
+    });
+    gsap.to('.creator', { opacity: 0, duration: 1, y: -40, stagger: 0.5, ease: 'elastic(1, 0.3)'});
+    gsap.to('.thinker', { opacity: 0, duration: 1, y: -50, stagger: 0.6, delay: 1.1});
 
+    // let options = {
+    //   strings: ['Designer', 
+    //   ' Creator',
+    //   'Thinker'
+    //   ],
+    //   typeSpeed: 40
+    // };
+    // let typed = new Typed('.typing', options);
   }
   render() {
     return (
@@ -40,18 +64,21 @@ export default class HomePage extends Component {
           <div className="col-xs-12 col-sm-12 col-md-4 col-lg-5 col-xl-5">
             <aside>
               <div className="align-content text-center">
-                <div className="d-c anim designer">Designer</div>
-                <div className="d-c anim creator">Creator</div>
-                <div className="d-c anim thinker">Thinker</div>
+                <div className="typing">
+                  <div className="d-c anim designer">Designer</div>
+                  <div className="d-c anim creator">Creator</div>
+                  <div className="d-c anim thinker">Thinker</div>
+                </div>
+                
 
                 <p className="myIntro">
-                  Currently, am <span>working</span> as a <span>Technical Support Engineer</span> 
-                  <span>@Microverse</span>.
+                  Hi, I am Daniel, currently, am <span className="work">working</span> as a <span className="tech">Technical Support Engineer</span> 
+                  <span className="micro">@Microverse</span>.I am also open to new challenges in software development roles.
                   Kindly follow my works, words and photos below
                 </p>
                 <div className="no-marg">
                     <div>
-                      <a href="https://github.com/addod19" className="s-links">
+                      <a href="https://github.com/addod19" className="s-links animTop">
                         <FaGithub />
                       </a>
                     </div>
@@ -84,29 +111,29 @@ export default class HomePage extends Component {
                 <div>
                   <h3 className="bskills">My Basic Skills</h3>
                 <ul className="text-center grid">
-                  <li className="ip-font">
+                  <li className="ip-font anim">
                     <FaHtml5 />
                     HTML
                   </li>
-                  <li className="ip-font">
+                  <li className="ip-font anim">
                     <FaCss3Alt />
                     CSS
                   </li>
-                  <li className="ip-font">
+                  <li className="ip-font anim">
                     <FaDatabase />
                     DATABASE
                   </li>
-                  <li className="ip-font">RUBY</li>
-                  <li className="ip-font">RUBY ON RAILS</li>
-                  <li className="ip-font">
+                  <li className="ip-font anim">RUBY</li>
+                  <li className="ip-font anim">RUBY ON RAILS</li>
+                  <li className="ip-font anim">
                     <FaJs />
                     JAVASCRIPT
                   </li>
-                  <li className="ip-font">
+                  <li className="ip-font anim">
                     <FaReact />
                     REACT/REDUX
                   </li>
-                  <li className="ip-font">TESTING (RSpec & Capybara, JEST) </li>
+                  <li className="ip-font anim">TESTING (RSpec & Capybara, JEST) </li>
                 </ul>
                 </div>
               </div>
@@ -117,17 +144,22 @@ export default class HomePage extends Component {
             <main>
               <div className="col-12">
                 <section className="text-center mwrap">
-                  <h2 className="t1-color anim">Facebook Clone</h2>
-                  <div className="ImgWrap animLeft">
+                  <Slide right>
+                  <h2 className="t1-color">Facebook Clone</h2>
+                  <div className="ImgWrap">
                     <img title="kindly click icons below" src={fbImg} alt="friendbook" className="style-img" />
                   </div>
-                  <div className="descp animRight">
+                  </Slide>
+                  <Slide left>
+                  <div className="descp">
                     Welcome to Friendbook, a Facebook-like social network application. This application has the 
                     functionalities of a social media application. Authorized users are able to create a post, 
                     comment on a post, like a post/comment, send and accept friend requests.
                   </div>
+                  </Slide>
+                  
                   <div className="technologies mt-3">
-                    <ul className="tech-items">
+                    <ul className="tech-items anim">
                       <li>Ruby on Rails</li>
                       <li>SCSS</li>
                       <li>RSpec</li>
@@ -142,16 +174,25 @@ export default class HomePage extends Component {
                 </section>
               </div>
               <hr></hr>
-              <div className="col-12">
+              <div className="col-12 tClass">
                 <section className="text-center">
+                  <Slide right>
                   <h2 className="t1-color"> Todo </h2>
-                  <img title="kindly click icons below" src={todoImg} alt="todo" className="style-img" />
+                  <div className="animLeft">
+                    <img title="kindly click icons below" src={todoImg} alt="todo" className="style-img" />
+                  </div>
+                  </Slide>
+                  
+                  <Slide left>
                   <div className="descp">
                     In this project we created a simple todo list applying the concepts of single responsibility 
                     and loosely coupled objects. We applied the revealing module design pattern.
                   </div>
+                  </Slide>
+
+                  
                   <div className="technologies mt-3">
-                    <ul className="tech-items m-auto">
+                    <ul className="tech-items m-auto anim">
                       <li className="mr-2">HTML</li>
                       <li className="mr-2">CSS</li>
                       <li className="mr-2">JavaScript</li>
@@ -167,13 +208,17 @@ export default class HomePage extends Component {
               <hr></hr>
               <div className="col-12">
                 <section className="text-center">
-                  <h2 className="t1-color">Tic Tac Toe </h2>
+                  <Slide right>
+                  <h2 className="t1-color anim">Tic Tac Toe </h2>
                   <img title="kindly click icons below" src={ticImg} alt="tictactoe" className="style-img" />
+                  </Slide>
+                  <Slide left>
                   <div className="descp">
                   This is a browser-based tic-tac-toe game created using vanilla Javascript.
                   </div>
+                  </Slide>
                   <div className="technologies mt-3">
-                    <ul className="tech-items m-auto">
+                    <ul className="tech-items m-auto anim">
                       <li className="mr-2">HTML</li>
                       <li className="mr-2">CSS</li>
                       <li className="mr-2">JavaScript</li>
@@ -188,14 +233,18 @@ export default class HomePage extends Component {
               <hr></hr>
               <div className="col-12">
                 <section className="text-center">
-                  <h2 className="t1-color">Event Brite</h2>
+                  <Slide right>
+                  <h2 className="t1-color anim">Event Brite</h2>
                   <img title="kindly click icons below" src={evImg} alt="private events" className="style-img" />
+                  </Slide>
+                  <Slide left>
                   <div className="descp">
                   Events manager application.In this project we built a site similar to a private Eventbrite which 
                   allows users to create events and then manage user signups. 
                   </div>
+                  </Slide>
                   <div className="technologies mt-3">
-                    <ul className="tech-items">
+                    <ul className="tech-items anim">
                       <li>Ruby on Rails</li>
                       <li>Bootstrap</li>
                       <li>SASS</li>
@@ -211,14 +260,18 @@ export default class HomePage extends Component {
               <hr></hr>
               <div className="col-12">
                 <section className="text-center">
-                  <h2 className="t1-color">Smashing Magazine </h2>
+                  <Slide right>
+                  <h2 className="t1-color anim">Smashing Magazine </h2>
                   <img title="kindly click icons below" src={smImg} alt="smash magazine" className="style-img" />
+                  </Slide>
+                  <Slide left>
                   <div className="descp">
                     Building a clone of smashingmagazine.com website, 
                     aligning all elements with float and flex or grid.
                   </div>
+                  </Slide>
                   <div className="technologies mt-3">
-                    <ul className="tech-items m-auto">
+                    <ul className="tech-items m-auto anim">
                       <li className="mr-2">HTML</li>
                       <li className="mr-2">CSS</li>
                     </ul>
@@ -232,14 +285,19 @@ export default class HomePage extends Component {
               <hr></hr>
               <div className="col-12">
                 <section className="text-center">
-                  <h2 className="t1-color">News Week </h2>
+                  <Slide right>
+                  <h2 className="t1-color anim">News Week </h2>
                   <img title="kindly click icons below" src={newsweekImg} alt="newsweek" className="style-img" />
+                  </Slide>
+                  <Slide left>
                   <div className="descp">
                     Building a Newsweek clone website using bootstrap classes. 
                     Considering mobile-first responsive design.
                   </div>
+                  </Slide>
+                  
                   <div className="technologies mt-3">
-                    <ul className="tech-items m-auto">
+                    <ul className="tech-items m-auto anim">
                       <li className="mr-2">HTML</li>
                       <li className="mr-2">CSS</li>
                       <li className="mr-2">Bootstrap</li>
@@ -254,13 +312,18 @@ export default class HomePage extends Component {
               <hr></hr>
               <div className="col-12">
                 <section className="text-center">
-                  <h2 className="t1-color">Simple Calculator </h2>
+                  <Slide right>
+                  <h2 className="t1-color anim">Simple Calculator </h2>
                   <img title="kindly click icons below" src={calImg} alt="calculator" className="style-img" />
+                  </Slide>
+                  <Slide left>
                   <div className="descp">
                     Building a basic functionality of a calculator with React
                   </div>
+                  </Slide>
+                  
                   <div className="technologies mt-3">
-                    <ul className="tech-items m-auto">
+                    <ul className="tech-items m-auto anim">
                       <li className="mr-2">React</li>
                       <li className="mr-2">CSS</li>
                       <li className="mr-2">JEST</li>
@@ -275,14 +338,19 @@ export default class HomePage extends Component {
               <hr></hr>
               <div className="col-12">
                 <section className="text-center">
-                  <h2 className="t1-color">Weather App </h2>
+                  <Slide right>
+                  <h2 className="t1-color anim">Weather App </h2>
                   <img title="kindly click icons below" src={weatherImg} alt="weather" className="style-img" />
+                  </Slide>
+                  <Slide left>
                   <div className="descp">
                     A weather app that pulls from the OpenWeatherMap API to allow users to search for 
                     and view the forecast in cities worldwide. Built with JavaScript
                   </div>
+                  </Slide>
+                  
                   <div className="technologies mt-3">
-                    <ul className="tech-items">
+                    <ul className="tech-items anim">
                       <li>HTML</li>
                       <li>CSS</li>
                       <li>JAVASCRIPT</li>
@@ -299,13 +367,18 @@ export default class HomePage extends Component {
               <hr></hr>
               <div className="col-12">
                 <section className="text-center">
-                  <h2 className="t1-color">Restaurant App</h2>
+                  <Slide right>
+                  <h2 className="t1-color anim">Restaurant App</h2>
                   <img title="kindly click icons below" src={restImg} alt="restaurant" className="style-img" />
+                  </Slide>
+                  <Slide left>
                   <div className="descp"> 
                     Building a restaurant app with ES6 classes, ES6 modules, and Webpack
                   </div>
+                  </Slide>
+                  
                   <div className="technologies mt-3">
-                    <ul className="tech-items m-auto">
+                    <ul className="tech-items m-auto anim">
                       <li className="mr-2">HTML</li>
                       <li className="mr-2">CSS</li>
                       <li className="mr-2">JAVASCRIPT</li>
@@ -320,14 +393,19 @@ export default class HomePage extends Component {
               <hr></hr>
               <div className="col-12">
                 <section className="text-center">
-                  <h2 className="t1-color">Library App</h2>
+                  <Slide right>
+                  <h2 className="t1-color anim">Library App</h2>
                   <img title="kindly click icons below" src={libImg} alt="library" className="style-img" />
+                  </Slide>
+                  <Slide left>
                   <div className="descp">
                     Building a book store library using vanilla Javascript. In this project, 
                     we demonstrate the use of Object and Object Constructors
                   </div>
+                  </Slide>
+                  
                   <div className="technologies mt-3">
-                    <ul className="tech-items">
+                    <ul className="tech-items anim">
                       <li>HTML</li>
                       <li>CSS</li>
                       <li>JAVASCRIPT</li>
@@ -340,11 +418,13 @@ export default class HomePage extends Component {
                   </div>
                 </section>
               </div>
-
+              {/* <Slide left> */}
               <div className="email">
+                <Flash >
                 <div className="i-large rotate">
                   <FaHandPointRight className="i-large" />
                 </div>
+                </Flash>
                 <div className="i-large">
                   email:
                   {' '}
@@ -353,7 +433,7 @@ export default class HomePage extends Component {
                   info@daniellarbiaddo.com
                 </div>
               </div>
-
+              {/* </Slide> */}
             </main>
           </div>
         </div>
