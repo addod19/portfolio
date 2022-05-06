9a2ce46ac2ff70ebd7bb26e57cf13ee213f8fda2
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2022-05-06 10:22:40 +0000

    Deploy build files

---
+++ precache-manifest.65a26352721c6d9eb48df91d963a7d89.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93dc597ea43751b33e89a5f1747f66b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e81ef56950899a2f740",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "99fe68b80baf86f23284",
+    "url": "/static/css/main.4d56d9c1.chunk.css"
+  },
+  {
+    "revision": "6e81ef56950899a2f740",
+    "url": "/static/js/2.e553c451.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/static/js/2.e553c451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99fe68b80baf86f23284",
+    "url": "/static/js/main.5078d7d4.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7432c21274eafbabebac225981c26e04",
+    "url": "/static/media/Group.7432c212.png"
+  },
+  {
+    "revision": "977da1261a845c37b97137ca7ad86422",
+    "url": "/static/media/group1.977da126.png"
+  }
+]);
